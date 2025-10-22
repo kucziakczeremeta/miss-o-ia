78fad50105b39fbcdd7c648a01797e3e1f7aba24
kucziakczeremeta <140640702+kucziakczeremeta@users.noreply.github.com>
2025-10-22 14:19:45 -0300

    dfghnjm

--- java/script.js
+++ java/script.js
@@ -12,3 +12,31 @@ let perguntasAtual;
 let historiaFinal = "";
 
 botaoIniciar.addEventListener('click,iniciaJogo');
+function iniciaJogo (){
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.computedStyleMap.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta();
+}
+
+function mostraPergunta (){
+    if (atual >= perguntas.length){
+        mostraResultados();
+        return;
+    }
+    perguntasAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntasAtual.enunciado;
+    caixaAlternativas.textContent = '';
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativas of perguntasAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativas.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+    }
+}
