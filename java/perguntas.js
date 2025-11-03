efb9be7daac4fcc09c14d59457af368c44566ed4
kucziakczeremeta <140640702+kucziakczeremeta@users.noreply.github.com>
2025-11-03 09:49:40 -0300

    zxc

--- java/perguntas.js
+++ java/perguntas.js
@@ -5,5 +5,5 @@ export const perguntas ={
             texto:"Inovação no entretenimento, educação e acessibilidade) com o imperativo de prevenir seu uso malicioso";
             afirmacao:"Os deepfakes ultrarrealistas são uma tecnologia de síntese de mídia crucial que deve ser protegida por seu potencial em democratizar a acessibilidade linguística (dublagens perfeitas), revolucionar o entretenimento (redução de custos e novas formas de arte) e aprimorar a educação (simulações históricas e de treinamento).",
         }
-    ]
+    ],
 } 
