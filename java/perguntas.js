13539f8d144d38fd0b1e3ab33d1ae0959046fc6e
kucziakczeremeta <140640702+kucziakczeremeta@users.noreply.github.com>
2025-11-03 09:48:18 -0300

    scvb

---
+++ java/perguntas.js
@@ -0,0 +1,9 @@
+export const perguntas ={
+    enunciado:"Como a legislação e as plataformas tecnológicas devem equilibrar o uso da Inteligência Artificial para a criação de deepfakes ultrarrealistas? O que vocẽ faria",
+    alternativas:[
+        {
+            texto:"Inovação no entretenimento, educação e acessibilidade) com o imperativo de prevenir seu uso malicioso";
+            afirmacao:"Os deepfakes ultrarrealistas são uma tecnologia de síntese de mídia crucial que deve ser protegida por seu potencial em democratizar a acessibilidade linguística (dublagens perfeitas), revolucionar o entretenimento (redução de custos e novas formas de arte) e aprimorar a educação (simulações históricas e de treinamento).",
+        }
+    ]
+} 
