71287c45a5f021d0a1987cccc388f5c0f1563f49
kucziakczeremeta <140640702+kucziakczeremeta@users.noreply.github.com>
2025-11-10 09:50:50 -0300

    zxcv

--- java/perguntas.js
+++ java/perguntas.js
@@ -1,9 +1,42 @@
-export const perguntas ={
-    enunciado:"Como a legislação e as plataformas tecnológicas devem equilibrar o uso da Inteligência Artificial para a criação de deepfakes ultrarrealistas? O que vocẽ faria",
-    alternativas:[
-        {
-            texto:"Inovação no entretenimento, educação e acessibilidade) com o imperativo de prevenir seu uso malicioso";
-            afirmacao:"Os deepfakes ultrarrealistas são uma tecnologia de síntese de mídia crucial que deve ser protegida por seu potencial em democratizar a acessibilidade linguística (dublagens perfeitas), revolucionar o entretenimento (redução de custos e novas formas de arte) e aprimorar a educação (simulações históricas e de treinamento).",
-        }
-    ],
-} 
+export const perguntas = [
+    {
+        enunciado:"Como a legislação e as plataformas tecnológicas devem equilibrar o uso da Inteligência Artificial para a criação de deepfakes ultrarrealistas? O que vocẽ faria",
+        alternativas:[
+            {
+                texto:"Inovação no entretenimento, educação e acessibilidade) com o imperativo de prevenir seu uso malicioso",
+                afirmacao:[
+                    "Os deepfakes ultrarrealistas são uma tecnologia de síntese de mídia crucial que deve ser protegida por seu potencial em democratizar a acessibilidade linguística (dublagens perfeitas), revolucionar o entretenimento (redução de custos e novas formas de arte) e aprimorar a educação (simulações históricas e de treinamento).",
+                ]
+                proxima: 1,
+            }
+            {
+                texto: "O imperativo de prevenir o uso malicioso da IA sempre falhará, pois a regulação é lenta demais.",
+                afirmacao: [
+                    "A justificativa desfavorável está incorreta porque ignora a viabilidade da ação coordenada e o valor intrínseco da inovação, focando apenas no pessimismo.",
+                ],
+                proxima: 2,
+            }
+        ]
+    },
+    {
+        enunciado:
+        alternativas:[
+
+            {
+                texto:
+                afirmacao:[
+
+                ],
+                proxima: 3,
+            }
+            {
+                texto:
+                afirmacao:[
+
+                ],
+                proxima: 4,
+            }
+        ]
+    }
+]
+  
