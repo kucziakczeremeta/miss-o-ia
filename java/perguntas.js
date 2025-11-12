4fd1ac65db65244a405f07f7f5fa27e726aecbb9
kucziakczeremeta <140640702+kucziakczeremeta@users.noreply.github.com>
2025-11-12 14:26:29 -0300

    afgg

--- java/perguntas.js
+++ java/perguntas.js
@@ -19,6 +19,42 @@ export const perguntas = [
         ]
     },
     {
+        enunciado:"O uso de Inteligência Artificial para criar vídeos falsos ultrarrealistas (deepfakes) é uma das maiores polêmicas do futuro.Esta tecnologia destrói a confiança no que vemos?"
+        alternativas:[
+
+            {
+                texto:"A principal controvérsia dos deepfakes reside no fato de serem vídeos ultrarrealistas que minam a confiança no conteúdo visual.",
+                afirmacao:[
+                    "O próprio contexto afirma que os deepfakes são "vídeos falsos ultrarrealistas" e que esta tecnologia "destrói a confiança no que vemos". Portanto, o caráter ultrarrealista e a consequente destruição da confiança são a essência da polêmica destacada no texto.",
+                ],
+                proxima: 3,
+            }
+            {
+                texto:"Deepfakes são vídeos falsos de baixa qualidade, e sua polêmica se deve apenas ao grande volume de conteúdo que eles geram.",
+                afirmacao:[
+                  "O texto descreve os deepfakes como "ultrarrealistas", o que contradiz a afirmação de que seriam de "baixa qualidade" e facilmente distinguíveis. A polêmica não se resume ao volume, mas sim ao perigo de não se conseguir mais discernir o que é real do que é falso.",
+                ],
+                proxima: 4,
+            }
+ {
+        enunciado:
+        alternativas:[
+
+            {
+                texto:
+                afirmacao:[
+
+                ],
+                proxima: 3,
+            }
+            {
+                texto:
+                afirmacao:[
+
+                ],
+                proxima: 4,
+            }
+          {
         enunciado:
         alternativas:[
 
