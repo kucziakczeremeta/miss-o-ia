b95c983e4dcd76e04e814626f67bda184aacb475
kucziakczeremeta <140640702+kucziakczeremeta@users.noreply.github.com>
2025-12-03 14:07:19 -0300

    2ert

--- aleatorio.js
+++ aleatorio.js
@@ -1,6 +1,6 @@
 const nomes = ["Poliana", "Daiane", "Eduardo", "Rodrigo", "Angela", "lisi", "Maria", "léo", "matheus"];
 export function aleatorio (lista){
-    const posicao = Math.floor(match.random()*lista.lenght);
+    const posicao = Math.floor(Math.random()*lista.lenght);
     return lista [posicao];
 }
 export const nome = aleatorio(nomes)
